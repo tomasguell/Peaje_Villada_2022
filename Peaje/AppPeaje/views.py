04445eacac2b2fcfbc142ef0145a1b058f49faf7
facuolivamar <facundoolivam@gmail.com>
2022-11-18 11:51:01 -0300

    base.html + urls

--- Peaje/AppPeaje/views.py
+++ Peaje/AppPeaje/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    context = {'key': 'hola'}
+    return render(request, 'AppPeaje/index.html', context=context)
+
