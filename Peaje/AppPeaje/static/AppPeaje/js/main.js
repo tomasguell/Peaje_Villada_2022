e3831ce24baf9cdd9e6b187a2d98227f537e4839
facuolivamar <facundoolivam@gmail.com>
2022-11-23 01:08:04 -0300

    sidebar

--- Peaje/AppPeaje/static/AppPeaje/js/main.js
+++ Peaje/AppPeaje/static/AppPeaje/js/main.js
@@ -1,8 +1,1 @@
-console.log("hola tio");
-function myFunction() {
-
-    $('#sidebarCollapse').on('click', function () {
-        $('#sidebar').toggleClass('active');
-    });
-
-};
+console.log('la terceraa');
