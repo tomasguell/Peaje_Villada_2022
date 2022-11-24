bb188640066314d9ddbc0ffa22416d9c8ce31cee
Tomas Guell <tomasguell123@gmail.com>
2022-11-24 20:14:25 -0300

    forms de turno creado

--- Peaje/AppPeaje/forms.py
+++ Peaje/AppPeaje/forms.py
@@ -1,4 +1,8 @@
 from django import forms
+from .models import turnos
 
-class NameForm(forms.Form):
-    your_name = forms.CharField(label='Your name', max_length=100)
+
+class NuevoTurno(forms.ModelForm):
+    class Meta:
+        model = turnos
+        fields = '__all__'
